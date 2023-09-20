3195d84700a362f97e8ecf1610fb0d5bcf936337
Nayra000 <youssefnayra26@gmail.com>
2023-09-20 13:56:15 +0300

    first commit

---
+++ project-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ytm90r10',
+    dataset: 'production'
+  }
+})
---
+++ project-backend/schemas/index.js
@@ -0,0 +1,8 @@
+import pin from './pin';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
+
+
+export const schemaTypes = [user ,pin ,comment,postedBy,save]
